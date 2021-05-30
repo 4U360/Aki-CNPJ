3eec87da5dcb3cc2d49802d0614eaaf8c065f86b
Henrique da Silva Santos <rique_dev@hotmail.com>
2021-05-30 01:07:35 -0300

    :sparkles: Adding missing files

--- akicnpj/downloader/simples.py
+++ akicnpj/downloader/simples.py
@@ -1,21 +1,18 @@
+from os import makedirs
 from .base import AkiDownloader
 from ..settings import ROOT, Path
-from os import makedirs
 
 
-class AkiEmpresaDownloader(AkiDownloader):
+class AkiSimplesDownloader(AkiDownloader):
 
     def download(self, path: Path = ROOT.parent.joinpath("data"), ignore_exists: bool = False):
         makedirs(str(path), exist_ok=True)
 
         for file in self.files:
-            if file.name.endswith("EMPRECSV.zip"):
-
+            if ".SIMPLES.CSV." in file.name and file.name.endswith(".zip"):
                 full_path = path.joinpath(file.name)
-
                 if not ignore_exists and full_path.exists():
                     self.logger.info(f"File {full_path} already exists, skipping...")
-                    yield full_path
                     continue
 
                 self.logger.info(f"Downloading {full_path} [{file.size_str}]")
--- tests/test_aki_downloader.py
+++ tests/test_aki_downloader.py
@@ -1,4 +1,13 @@
-from akicnpj.downloader.empresa import AkiEmpresaDownloader
+from akicnpj.downloader import *
 
 if __name__ == "__main__":
     AkiEmpresaDownloader().download()
+    AkiEstabelecimentoDownloader().download()
+    AkiSocioDownloader().download()
+    AkiSimplesDownloader().download()
+    AkiCnaeDownloader().download()
+    AkiMunicipioDownloader().download()
+    AkiNaturezaJuridicaDownloader().download()
+    AkiPaisDownloader().download()
+    AkiQSADownloader().download()
+    AkiSituacaoCadastralDownloader().download()
